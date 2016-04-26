d19ec1c0fd72234ce7b105658fd1f39af2118698
kimiyoung <kimiyoung@yeah.net>
2016-04-26 15:51:31 -0400

    fix index

--- soft_att_lstm.lua
+++ soft_att_lstm.lua
@@ -210,7 +210,7 @@ function M.train(model, epoch, opt, batches, val_batches, optim_state, dataloade
                 for k = 1, input_text:size()[1] do
                     local caption = ''
                     for t = 1, seq_len do
-                        local word_index = max_pred[t][l]
+                        local word_index = max_pred[t][k]
                         if word_index == anno_utils.STOP_NUM then break end
                         if caption ~= '' then
                             caption = caption .. ' ' .. index2word[word_index]
