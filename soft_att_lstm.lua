ebb083c1b91e3093b8655be8b7215008b5e396bd
kimiyoung <kimiyoung@yeah.net>
2016-04-26 00:43:07 -0400

    debug: print size

--- soft_att_lstm.lua
+++ soft_att_lstm.lua
@@ -139,6 +139,8 @@ function M.train(model, epoch, opt, batches, optim_state, dataloader)
                 end
                 
                 -- backprop through LSTM timestep
+                print(embeddings[t]:size(), att_seq:size(), lstm_c[t - 1]:size(), lstm_h[t - 1]:size(), dlstm_c[t]:size(),
+                    dlstm_h[t]:size())
                 dembeddings[t], _, dlstm_c[t-1], dlstm_h[t-1] = unpack(clones.soft_att_lstm[t]:
                     backward({embeddings[t], att_seq, lstm_c[t-1], lstm_h[t-1]},
                     {dlstm_c[t], dlstm_h[t]}))                                           -- lstm backward
