325370477ab3ac101557e8a7bbdf3ef5b9a3aa05
kimiyoung <kimiyoung@yeah.net>
2016-04-29 20:29:00 -0400

    reasoning net with init fc7 and no input

--- reasoning.lua
+++ reasoning.lua
@@ -123,13 +123,91 @@ function M.soft_att_lstm_concat(opt)
     return nn.gModule({x, att_seq, prev_c, prev_h}, {next_c, next_h})
 end
 
+-- Attention model, concat hidden and image feature
+function M.soft_att_lstm_concat_nox(opt)
+    -- Model parameters
+    local feat_size = opt.feat_size
+    local att_size = opt.att_size
+    local rnn_size = opt.lstm_size
+    local input_size = opt.fc7_size
+    local att_hid_size = opt.att_hid_size
+
+    local att_seq = nn.Identity()()   -- batch * att_size * feat_size -- the image patches
+    local prev_c = nn.Identity()()
+    local prev_h = nn.Identity()()
+
+    ------------ Attention part --------------------
+    local att = nn.View(-1, feat_size)(att_seq)         -- (batch * att_size) * feat_size
+    local att_h, dot
+
+    if att_hid_size > 0 then
+        att = nn.Linear(feat_size, att_hid_size)(att)       -- (batch * att_size) * att_hid_size
+        att = nn.View(-1, att_size, att_hid_size)(att)      -- batch * att_size * att_hid_size
+        att_h = nn.Linear(rnn_size, att_hid_size)(prev_h)   -- batch * att_hid_size
+        att_h = nn.Replicate(att_size, 2)(att_h)            -- batch * att_size * att_hid_size
+        dot = nn.CAddTable(){att_h, att}                    -- batch * att_size * att_hid_size
+        dot = nn.Tanh()(dot)                                -- batch * att_size * att_hid_size
+        dot = nn.View(-1, att_hid_size)(dot)                -- (batch * att_size) * att_hid_size
+        dot = nn.Linear(att_hid_size, 1)(dot)               -- (batch * att_size) * 1
+        dot = nn.View(-1, att_size)(dot)                             -- batch * att_size
+    else
+        att = nn.Linear(feat_size, 1)(att)                  -- (batch * att_size) * 1
+        att = nn.View(-1, att_size)(att)                    -- batch * att_size
+        att_h = nn.Linear(rnn_size, 1)(prev_h)              -- batch * 1
+        att_h = nn.Replicate(att_size, 2)(att_h)            -- batch * att_size * 1
+        att_h = nn.Squeeze()(att_h)                         -- batch * att_size
+        dot = nn.CAddTable(){att_h, att}                    -- batch * att_size
+    end
+
+    local weight = nn.SoftMax()(dot)
+        
+    local att_seq_t = nn.Transpose({2, 3})(att_seq)     -- batch * rnn_size * att_size
+    local att_res = nn.MixtureTable(3){weight, att_seq_t}      -- batch * rnn_size <- (batch * att_size, batch * rnn_size * att_size)
+
+    -------------- End of attention part -----------
+    
+    --- Input to LSTM
+    local att_add = nn.Linear(feat_size, 4 * rnn_size)(att_res)   -- batch * (4*rnn_size) <- batch * feat_size
+
+    ------------- LSTM main part --------------------
+    local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h)
+    
+    -- test
+    -- local prev_all_input_sums = nn.CAddTable()({i2h, h2h})
+    -- local all_input_sums = nn.CAddTable()({prev_all_input_sums, att_add})
+
+    local all_input_sums = nn.CAddTable()({h2h, att_add})
+
+    local sigmoid_chunk = nn.Narrow(2, 1, 3 * rnn_size)(all_input_sums)
+    sigmoid_chunk = nn.Sigmoid()(sigmoid_chunk)
+    local in_gate = nn.Narrow(2, 1, rnn_size)(sigmoid_chunk)
+    local forget_gate = nn.Narrow(2, rnn_size + 1, rnn_size)(sigmoid_chunk)
+    local out_gate = nn.Narrow(2, 2 * rnn_size + 1, rnn_size)(sigmoid_chunk)
+
+    local in_transform = nn.Narrow(2, 3 * rnn_size + 1, rnn_size)(all_input_sums)
+    in_transform = nn.Tanh()(in_transform)
+
+    local next_c = nn.CAddTable()({
+        nn.CMulTable()({forget_gate, prev_c}),
+        nn.CMulTable()({in_gate,     in_transform})
+    })
+    local next_h = nn.CMulTable()({out_gate, nn.Tanh()(next_c)}) -- batch * rnn_size
+    
+    return nn.gModule({att_seq, prev_c, prev_h}, {next_c, next_h})
+end
+
 -------------------------------------
 -- Train the model for an epoch
 -- INPUT
 -- batches: {{id, caption}, ..., ...}
 -------------------------------------
 function M.train(model, opt, batches, val_batches, optim_state, dataloader)
-    local params, grad_params = model_utils.combine_all_parameters(model.emb, model.soft_att_lstm, model.lstm, model.softmax)
+    local params, grad_params
+    if opt.lstm_size ~= opt.fc7_size then
+        model_utils.combine_all_parameters(model.emb, model.soft_att_lstm, model.lstm, model.softmax, model.linear)
+    else
+        model_utils.combine_all_parameters(model.emb, model.soft_att_lstm, model.lstm, model.softmax)
+    end
     local clones = {}
     anno_utils = dataloader.anno_utils
     
@@ -154,9 +232,16 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
         end
         grad_params:zero()
 
+        local image_map
+        if opt.fc7_size ~= opt.lstm_size then
+            image_map = model.linear:forward(fc7_images)
+        else
+            image_map = fc7_images
+        end
+
         local zero_tensor = torch.zeros(input_text:size()[1], opt.lstm_size):cuda()
-        local reason_c = {[0] = zero_tensor}
-        local reason_h = {[0] = zero_tensor}
+        local reason_c = {[0] = image_map}
+        local reason_h = {[0] = image_map}
         local embeddings, lstm_c, lstm_h, predictions = {}, {}, {}, {}
         local loss = 0
         local seq_len = math.min(input_text:size()[2], max_t)
@@ -164,7 +249,7 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
         
         for t = 1, reason_len do
             reason_c[t], reason_h[t] = unpack(clones.soft_att_lstm[t]:
-                forward{fc7_images, att_seq, reason_c[t - 1], reason_h[t - 1]})
+                forward{att_seq, reason_c[t - 1], reason_h[t - 1]})
         end
 
         lstm_c[0] = reason_c[reason_len]
@@ -194,8 +279,8 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
             dreason_c[reason_len] = dlstm_c[0]
             dreason_h[reason_len] = dlstm_h[0]
             for t = reason_len, 1, -1 do
-                _, _, dreason_c[t - 1], dreason_h[t - 1] = unpack(clones.soft_att_lstm[t]:
-                    backward({fc7_images, att_seq, reason_c[t - 1], reason_h[t - 1]},
+                _, dreason_c[t - 1], dreason_h[t - 1] = unpack(clones.soft_att_lstm[t]:
+                    backward({att_seq, reason_c[t - 1], reason_h[t - 1]},
                     {dreason_c[t], dreason_h[t]}))
             end
         end
@@ -238,16 +323,22 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
                     local j2 = math.min(#dataloader.val_set, j1 + opt.val_batch_size)
                     att_seq, fc7_images = dataloader:gen_test_data(j1, j2)
 
-                    local zero_tensor = torch.zeros(att_seq:size()[1], opt.lstm_size):cuda()
-                    local reason_c = {[0] = zero_tensor}
-                    local reason_h = {[0] = zero_tensor}
+                    local image_map
+                    if opt.lstm_size ~= opt.fc7_size then
+                        image_map = model.linear:forward(fc7_images)
+                    else
+                        image_map = fc7_images
+                    end
+
+                    local reason_c = {[0] = image_map}
+                    local reason_h = {[0] = image_map}
                     local embeddings, lstm_c, lstm_h, predictions, max_pred = {}, {}, {}, {}, {}
                     local reason_len = opt.reason_step
                     local seq_len = max_t
                     
                     for t = 1, reason_len do
                         reason_c[t], reason_h[t] = unpack(clones.soft_att_lstm[t]:
-                            forward{fc7_images, att_seq, reason_c[t - 1], reason_h[t - 1]})
+                            forward{att_seq, reason_c[t - 1], reason_h[t - 1]})
                     end
 
                     lstm_c[0] = reason_c[reason_len]
@@ -309,10 +400,14 @@ end
 function M.create_model(opt)
     local model = {}
     model.emb = nn.LookupTable(opt.word_cnt, opt.emb_size)
-    model.soft_att_lstm = M.soft_att_lstm_concat(opt)
+    -- model.soft_att_lstm = M.soft_att_lstm_concat(opt)
+    model.soft_att_lstm = M.soft_att_lstm_concat_nox(opt)
     model.lstm = M.lstm(opt)
     model.softmax = nn.Sequential():add(nn.Linear(opt.lstm_size, opt.word_cnt)):add(nn.LogSoftMax())
     model.criterion = nn.ClassNLLCriterion()
+    if opt.fc7_size ~= opt.lstm_size then
+        model.linear = nn.Linear(opt.fc7_size, opt.lstm_size)
+    end
     
     if opt.nGPU > 0 then
         model.emb:cuda()
@@ -320,6 +415,9 @@ function M.create_model(opt)
         model.lstm:cuda()
         model.softmax:cuda()
         model.criterion:cuda()
+        if opt.fc7_size ~= opt.lstm_size then
+            model.linear:cuda()
+        end
     end
     return model
 end
