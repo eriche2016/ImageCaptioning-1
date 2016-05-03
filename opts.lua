f7336bba375890da9e8af52f9fd4f980cd88b1cc
kimiyoung <kimiyoung@yeah.net>
2016-05-03 10:55:47 -0400

    test unk

--- opts.lua
+++ opts.lua
@@ -19,7 +19,7 @@ function M.parse(arg)
     cmd:option('-test_size', 4000, 'Test set size')
 
     cmd:option('-use_attention', true, 'Use attention or not')
-    cmd:option('-use_noun', true, 'Use noun or not')
+    cmd:option('-use_noun', false, 'Use noun or not')
 
     -- cmd:option('-use_reasoning', true, 'Use reasoning. Will use attention in default.')
     cmd:option('-model_pack', 'reason_att', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
