d3a73bab016a82fa433211d78961fd150b39e869
kimiyoung <kimiyoung@yeah.net>
2016-07-20 17:36:45 -0400

    no update

--- reason_att_copy_finetune.lua
+++ reason_att_copy_finetune.lua
@@ -374,7 +374,7 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
         local index = torch.randperm(#batches)
         for i = 1, #batches do
             jpg, input_text, output_text, noun_list = dataloader:gen_train_jpg(batches[index[i]])
-            optim.adagrad(feval, params, optim_state)
+            -- optim.adagrad(feval, params, optim_state)
             
             ----------------- Evaluate the model in validation set ----------------
             if i == 1 or i % opt.loss_period == 0 then
