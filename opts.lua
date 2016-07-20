7d064df0d931b95a2b0c5fed4a286b9829953ca1
kimiyoung <kimiyoung@yeah.net>
2016-07-20 17:32:25 -0400

    fix

--- opts.lua
+++ opts.lua
@@ -72,7 +72,7 @@ function M.parse(arg)
 
     cmd:option('-load_file', true, 'whether load model file?')
     cmd:option('-load_file_name', 'copy.all.val.8.w10.noun.model')
-    cmd:option('-load_conv5_name', 'vgg_input_conv5_cudnn.t7', )
+    cmd:option('-load_conv5_name', 'vgg_input_conv5_cudnn.t7')
     cmd:option('-load_fc7_name', 'vgg_conv5_fc7_cudnn.t7')
 
     cmd:option('-train_only', false, 'if true then use 80k, else use 110k')
