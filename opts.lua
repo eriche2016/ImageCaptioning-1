5ae4f7c00b500bac648aaa115cf6bc872e9f6723
kimiyoung <kimiyoung@yeah.net>
2016-07-21 12:26:16 -0400

    fine tune starting from 6000

--- opts.lua
+++ opts.lua
@@ -25,7 +25,7 @@ function M.parse(arg)
     cmd:option('-reason_weight', 10.0, 'weight of reasoning loss')
 
     -- cmd:option('-use_reasoning', true, 'Use reasoning. Will use attention in default.')
-    cmd:option('-model_pack', 'reason_att_copy', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
+    cmd:option('-model_pack', 'reason_att_copy_finetune', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
     cmd:option('-reason_step', 8, 'Reasoning steps before the decoder')
 
     ------------ General options --------------------
@@ -65,15 +65,15 @@ function M.parse(arg)
     cmd:option('-truncate', 30, 'Text longer than this size gets truncated. -1 for no truncation.')
     cmd:option('-max_eval_batch', 50, 'max number of instances when calling comp error. 20000 = 4000 * 5')
 
-    cmd:option('-save_file', true, 'whether save model file?')
+    cmd:option('-save_file', false, 'whether save model file?')
     cmd:option('-save_file_name', 'copy.server.init.vgg.all.val.8.w10.noun.model', 'file name for saving model')
     cmd:option('-save_conv5_name', 'copy.server.lr1e-4.fine.vgg.conv5.model')
     cmd:option('-save_fc7_name', 'copy.server.lr1e-4.fine.vgg.fc7.model')
 
-    cmd:option('-load_file', false, 'whether load model file?')
-    cmd:option('-load_file_name', 'copy.server.vgg.all.val.8.w10.noun.model')
-    cmd:option('-load_conv5_name', 'vgg_input_conv5_cudnn.t7')
-    cmd:option('-load_fc7_name', 'vgg_conv5_fc7_cudnn.t7')
+    cmd:option('-load_file', true, 'whether load model file?')
+    cmd:option('-load_file_name', 'copy.server.init6000.vgg.all.val.8.w10.noun.model')
+    cmd:option('-load_conv5_name', 'vgg_input_conv5_cunn.t7')
+    cmd:option('-load_fc7_name', 'vgg_conv5_fc7_cunn.t7')
 
     cmd:option('-train_only', false, 'if true then use 80k, else use 110k')
     cmd:option('-early_stop', 'cider', 'can be cider or bleu')
