001c76a22fb945dccb44aa208631d53137d5f493
kimiyoung <kimiyoung@yeah.net>
2016-07-21 00:10:05 -0400

    debug: test again

--- opts.lua
+++ opts.lua
@@ -25,7 +25,7 @@ function M.parse(arg)
     cmd:option('-reason_weight', 10.0, 'weight of reasoning loss')
 
     -- cmd:option('-use_reasoning', true, 'Use reasoning. Will use attention in default.')
-    cmd:option('-model_pack', 'reason_att_copy_finetune', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
+    cmd:option('-model_pack', 'reason_att_copy', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
     cmd:option('-reason_step', 8, 'Reasoning steps before the decoder')
 
     ------------ General options --------------------
@@ -61,11 +61,11 @@ function M.parse(arg)
     -- cmd:option('-loss_period', 2400, 'Every given number of iterations, compute the loss on train and test')
     cmd:option('-batch_size', 32, 'Batch size in SGD')
     cmd:option('-val_batch_size', 10, 'Batch size for testing')
-    cmd:option('-LR', 1e-6, 'Initial learning rate') -- 0.01
+    cmd:option('-LR', 0, 'Initial learning rate') -- 0.01
     cmd:option('-truncate', 30, 'Text longer than this size gets truncated. -1 for no truncation.')
     cmd:option('-max_eval_batch', 50, 'max number of instances when calling comp error. 20000 = 4000 * 5')
 
-    cmd:option('-save_file', true, 'whether save model file?')
+    cmd:option('-save_file', false, 'whether save model file?')
     cmd:option('-save_file_name', 'copy.server.fine.vgg.all.val.8.w10.noun.model', 'file name for saving model')
     cmd:option('-save_conv5_name', 'copy.server.fine.vgg.conv5.model')
     cmd:option('-save_fc7_name', 'copy.server.fine.vgg.fc7.model')
