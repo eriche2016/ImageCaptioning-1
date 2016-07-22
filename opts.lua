4ad101cdbb3a8910825af1e88625b0064715f5dd
kimiyoung <kimiyoung@yeah.net>
2016-07-21 23:42:52 -0400

    fix model

--- opts.lua
+++ opts.lua
@@ -71,7 +71,7 @@ function M.parse(arg)
     cmd:option('-save_fc7_name', 'test.clean.fc7.model')
 
     cmd:option('-load_file', true, 'whether load model file?')
-    cmd:option('-load_file_name', 'concat.1024.512.model')
+    cmd:option('-load_file_name', 'att.all.val.8.model')
     cmd:option('-load_conv5_name', 'vgg_input_conv5_cunn.t7')
     cmd:option('-load_fc7_name', 'vgg_conv5_fc7_cunn.t7')
 
