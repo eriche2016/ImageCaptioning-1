ba40ba7fd5f0508da2386d70809ef36f453a83bf
kimiyoung <kimiyoung@yeah.net>
2016-07-21 21:49:32 -0400

    cg

--- reason_att_copy_finetune.lua
+++ reason_att_copy_finetune.lua
@@ -203,6 +203,7 @@ end
 -- batches: {{id, caption}, ..., ...}
 -------------------------------------
 function M.train(model, opt, batches, val_batches, optim_state, dataloader)
+    collectgarbage()
     local input2conv5 = torch.load('models/' .. opt.load_conv5_name)
     local conv52fc7 = torch.load('models/' .. opt.load_fc7_name)
 
@@ -228,7 +229,6 @@ function M.train(model, opt, batches, val_batches, optim_state, dataloader)
     for t = 1, opt.reason_step do
         table.insert(model_list, model.soft_att_lstm[t])
     end
-    -- model_list = {input2conv5, conv52fc7}
 
     params, grad_params = model_utils.combine_all_parameters(unpack(model_list))
     local clones = {}
