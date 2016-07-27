d8302416d8acaf09ecd23a34f873db63eace761f
kimiyoung <kimiyoung@yeah.net>
2016-07-27 17:16:02 -0400

    eval offline test set; sanity check

--- opts.lua
+++ opts.lua
@@ -99,8 +99,8 @@ function M.parse(arg)
     cmd:option('-val_max_len', 20, 'Max length in validation state')
 
     cmd:option('-test_mode', false, 'eval on test set if true')
-    cmd:option('-server_train_mode', false, 'eval on test of val, and use the rest for training')
-    cmd:option('-server_test_mode', true, 'eval on server test set if true; if true then test_mode will be false.')
+    cmd:option('-server_train_mode', true, 'eval on test of val, and use the rest for training')
+    cmd:option('-server_test_mode', false, 'eval on server test set if true; if true then test_mode will be false.')
     
     local opt = cmd:parse(arg or {})
     opt.eval_period = math.floor(3000 * 32 / opt.batch_size) * 2
