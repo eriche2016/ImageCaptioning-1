75b6b961c3fa14254c6bfcbd5e35acf54e2035b8
kimiyoung <kimiyoung@yeah.net>
2016-07-28 10:07:16 -0400

    gen js

--- gen_js.py
+++ gen_js.py
@@ -1,20 +1,20 @@
 
 import json
 
-# data = []
-# for i in range(1, 3):
-#     for line in open('server_test/captions{}.txt'.format(i)):
-#         inputs = line.strip().split('\t')
-#         data.append({'image_id': int(inputs[0]), 'caption': inputs[1]})
-# json.dump(data, open('server_test/captions_test2014_review_results.json', 'w'))
-
-
-from os import listdir
-from os.path import isfile, join
-
 data = []
-for filename in listdir('data/val2014_features_googlenet'):
-    if filename.endswith('dat'):
-        id = int(filename.split('_')[-1].split('.')[0])
-        data.append({'image_id': id, 'caption': ''})
-json.dump(data, open('server_test/captions_val2014_review_results.json', 'w'))
+for i in range(1, 3):
+    for line in open('server_test/vgg16.{}.txt'.format(i)):
+        inputs = line.strip().split('\t')
+        data.append({'image_id': int(inputs[0]), 'caption': inputs[1]})
+json.dump(data, open('server_test/captions_test2014_vgg16_results.json', 'w'))
+
+
+# from os import listdir
+# from os.path import isfile, join
+
+# data = []
+# for filename in listdir('data/val2014_features_googlenet'):
+#     if filename.endswith('dat'):
+#         id = int(filename.split('_')[-1].split('.')[0])
+#         data.append({'image_id': id, 'caption': ''})
+# json.dump(data, open('server_test/captions_val2014_review_results.json', 'w'))
