6d72155691ded8f7b76d4c593fe53e05b0b26719
kimiyoung <kimiyoung@yeah.net>
2016-08-25 12:16:55 -0400

    fine conv 01e-6

--- opts.lua
+++ opts.lua
@@ -27,18 +27,18 @@ function M.parse(arg)
     cmd:option('-gen_weight', 6.0) -- 30
 
     -- cmd:option('-use_reasoning', true, 'Use reasoning. Will use attention in default.')
-    cmd:option('-model_pack', 'reason_att_copy', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
+    cmd:option('-model_pack', 'reason_att_copy_fineconv', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
     cmd:option('-reason_step', 8, 'Reasoning steps before the decoder')
 
     ------------ General options --------------------
     cmd:option('-data', 'data/', 'Path to dataset')
-    -- cmd:option('-train_feat', 'train2014_features_vgg_vd19_conv5', 'Path to pre-extracted training image feature')
-    -- cmd:option('-val_feat', 'val2014_features_vgg_vd19_conv5', 'Path to pre-extracted validation image feature')
-    -- cmd:option('-test_feat', 'test2014_features_vgg_vd19_conv5_2nd', 'Path to pre-extracted test image feature')
+    cmd:option('-train_feat', 'train2014_features_vgg_vd19_conv5', 'Path to pre-extracted training image feature')
+    cmd:option('-val_feat', 'val2014_features_vgg_vd19_conv5', 'Path to pre-extracted validation image feature')
+    cmd:option('-test_feat', 'test2014_features_vgg_vd19_conv5_2nd', 'Path to pre-extracted test image feature')
 
-    cmd:option('-train_feat', 'train2014_vgg_vd16_conv5', 'Path to pre-extracted training image feature')
-    cmd:option('-val_feat', 'val2014_vgg_vd16_conv5', 'Path to pre-extracted validation image feature')
-    cmd:option('-test_feat', 'test2014_vgg_vd16_conv5', 'Path to pre-extracted test image feature')
+    -- cmd:option('-train_feat', 'train2014_vgg_vd16_conv5', 'Path to pre-extracted training image feature')
+    -- cmd:option('-val_feat', 'val2014_vgg_vd16_conv5', 'Path to pre-extracted validation image feature')
+    -- cmd:option('-test_feat', 'test2014_vgg_vd16_conv5', 'Path to pre-extracted test image feature')
 
     -- cmd:option('-train_fc7', 'train2014_features_vgg_vd19_fc7', 'Path to pre-extracted training fully connected 7')
     -- cmd:option('-val_fc7', 'val2014_features_vgg_vd19_fc7', 'Path to pre-extracted validation fully connected 7')
@@ -71,17 +71,17 @@ function M.parse(arg)
     -- cmd:option('-loss_period', 2400, 'Every given number of iterations, compute the loss on train and test')
     cmd:option('-batch_size', 32, 'Batch size in SGD')
     cmd:option('-val_batch_size', 10, 'Batch size for testing')
-    cmd:option('-LR', 1e-2, 'Initial learning rate') -- 1e-2
+    cmd:option('-LR', 0, 'Initial learning rate') -- 1e-2
     cmd:option('-cnn_LR', 1e-6, 'Learning rate for cnn') -- 1e-2
     cmd:option('-truncate', 30, 'Text longer than this size gets truncated. -1 for no truncation.') -- 30
     cmd:option('-max_eval_batch', 50, 'max number of instances when calling comp error. 20000 = 4000 * 5')
 
     cmd:option('-save_file', true, 'whether save model file?')
-    cmd:option('-save_file_name', 'offdev.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
+    cmd:option('-save_file_name', 'fienconv01e-6.offdev.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
     cmd:option('-save_conv5_name', '12000.1e-5.fine.conv5.model')
     cmd:option('-save_fc7_name', '12000.1e-5.fine.fc7.model')
 
-    cmd:option('-load_file', false, 'whether load model file?')
+    cmd:option('-load_file', true, 'whether load model file?')
     cmd:option('-load_vgg_file', false)
     cmd:option('-load_file_name', 'offdev.conv519.seed13.backfixed.reason.copy.vgg16.model')
     cmd:option('-load_conv5_name', 'vgg_input_conv5_cunn.t7')
@@ -116,7 +116,7 @@ function M.parse(arg)
     if opt.server_test_mode then opt.test_mode = false end
     if opt.server_train_mode then opt.test_mode = false end
     opt.jpg = false
-    if opt.model_pack == 'reason_att_copy_finetune' or opt.model_pack == 'reason_att_copy_conv5' then opt.jpg = true end
+    if opt.model_pack == 'reason_att_copy_finetune' or opt.model_pack == 'reason_att_copy_fineboth' or opt.model_pack == 'reason_att_copy_fineconv' then opt.jpg = true end
     opt.model = opt.load_file_name
     return opt
 end
