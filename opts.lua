37a38d670fb9e25195475122b24fcc8465ef87c7
kimiyoung <kimiyoung@yeah.net>
2016-08-26 11:08:19 -0400

    run gdrop0.1 finefc7 01e-6

--- opts.lua
+++ opts.lua
@@ -71,13 +71,13 @@ function M.parse(arg)
     -- cmd:option('-loss_period', 2400, 'Every given number of iterations, compute the loss on train and test')
     cmd:option('-batch_size', 32, 'Batch size in SGD')
     cmd:option('-val_batch_size', 10, 'Batch size for testing')
-    cmd:option('-LR', 1e-6, 'Initial learning rate') -- 1e-2
+    cmd:option('-LR', 0, 'Initial learning rate') -- 1e-2
     cmd:option('-cnn_LR', 1e-6, 'Learning rate for cnn') -- 1e-2
     cmd:option('-truncate', 30, 'Text longer than this size gets truncated. -1 for no truncation.') -- 30
     cmd:option('-max_eval_batch', 50, 'max number of instances when calling comp error. 20000 = 4000 * 5')
 
     cmd:option('-save_file', true, 'whether save model file?')
-    cmd:option('-save_file_name', 'fine1e-61e-6.gen_drop0.1.offdev.conv519.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
+    cmd:option('-save_file_name', 'fine01e-6.gen_drop0.1.offdev.conv519.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
     cmd:option('-save_conv5_name', '12000.1e-5.fine.conv5.model')
     cmd:option('-save_fc7_name', '12000.1e-5.fine.fc7.model')
 
