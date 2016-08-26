40b33976ad8d05e7f1458d4cab3aafe65d8ca6e0
kimiyoung <kimiyoung@yeah.net>
2016-08-25 23:05:50 -0400

    run gen drop 0.1 offdev

--- opts.lua
+++ opts.lua
@@ -27,7 +27,7 @@ function M.parse(arg)
     cmd:option('-gen_weight', 6.0) -- 30
 
     -- cmd:option('-use_reasoning', true, 'Use reasoning. Will use attention in default.')
-    cmd:option('-model_pack', 'reason_att_copy_fineconv', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
+    cmd:option('-model_pack', 'reason_att_copy', 'the model package to use, can be reason_att, reasoning, or soft_att_lstm')
     cmd:option('-reason_step', 8, 'Reasoning steps before the decoder')
 
     ------------ General options --------------------
@@ -71,17 +71,17 @@ function M.parse(arg)
     -- cmd:option('-loss_period', 2400, 'Every given number of iterations, compute the loss on train and test')
     cmd:option('-batch_size', 32, 'Batch size in SGD')
     cmd:option('-val_batch_size', 10, 'Batch size for testing')
-    cmd:option('-LR', 0, 'Initial learning rate') -- 1e-2
+    cmd:option('-LR', 1e-2, 'Initial learning rate') -- 1e-2
     cmd:option('-cnn_LR', 1e-7, 'Learning rate for cnn') -- 1e-2
     cmd:option('-truncate', 30, 'Text longer than this size gets truncated. -1 for no truncation.') -- 30
     cmd:option('-max_eval_batch', 50, 'max number of instances when calling comp error. 20000 = 4000 * 5')
 
     cmd:option('-save_file', true, 'whether save model file?')
-    cmd:option('-save_file_name', 'fienconv01e-7.offdev.conv519.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
+    cmd:option('-save_file_name', 'gen_drop0.1.offdev.conv519.seed13.backfixed.reason.copy.vgg16.model', 'file name for saving model')
     cmd:option('-save_conv5_name', '12000.1e-5.fine.conv5.model')
     cmd:option('-save_fc7_name', '12000.1e-5.fine.fc7.model')
 
-    cmd:option('-load_file', true, 'whether load model file?')
+    cmd:option('-load_file', false, 'whether load model file?')
     cmd:option('-load_vgg_file', false)
     cmd:option('-load_file_name', 'offdev.conv519.seed13.backfixed.reason.copy.vgg16.model')
     cmd:option('-load_conv5_name', 'vgg_input_conv5_cunn.t7')
@@ -95,7 +95,7 @@ function M.parse(arg)
     cmd:option('-cnn_dropout', true) -- true
     cmd:option('-normalize', false) -- false
     cmd:option('-reason_dropout', 0.0)
-    cmd:option('-gen_dropout', 0.0)
+    cmd:option('-gen_dropout', 0.1)
     cmd:option('-load_glove', false) -- false
     
     ------------ Evaluation options --------------------
